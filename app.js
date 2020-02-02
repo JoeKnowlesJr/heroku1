a32375b0076952d04a3760865c5bcea4abb5bdb9
Joe Knowles <josephbaderknowlesjr@gmail.com>
2020-02-02 02:05:10 -0600

    'testing'

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', frontEnd);
+
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
