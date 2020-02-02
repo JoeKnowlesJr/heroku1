6b3cb6a4c38172d97ef4fce334054069e5777638
Joe Knowles <josephbaderknowlesjr@gmail.com>
2020-02-02 02:07:33 -0600

    'testing'

--- routes/frontend.js
+++ routes/frontend.js
@@ -4,7 +4,7 @@ const router = express.Router();
 // application -------------------------------------------------------------
 router.get('*', function(req, res) {
     console.log('Getting frontend');
-    res.sendfile('../frontend/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+    res.sendfile('../public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
 });
 
 module.exports = router;
