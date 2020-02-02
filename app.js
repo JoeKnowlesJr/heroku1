0330cd21a3a51046b31559cf294e816e8038d478
Joe Knowles <josephbaderknowlesjr@gmail.com>
2020-02-02 12:39:17 -0600

    'testing'

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', frontEnd);
+app.use('', frontEnd);
 
 app.use('/users', usersRouter);
 
