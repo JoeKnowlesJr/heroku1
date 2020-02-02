dc5cce1965009711774f5eeafb834b2900f7a2de
Joe Knowles <josephbaderknowlesjr@gmail.com>
2020-02-02 11:40:44 -0600

    'testing'

--- routes/frontend.js
+++ routes/frontend.js
@@ -4,7 +4,9 @@ const router = express.Router();
 // application -------------------------------------------------------------
 router.get('*', function(req, res) {
     console.log('Getting frontend');
-    res.sendFile(path.join(__dirname, '../public/jkp/src', 'index.html')); // load the single view file (angular will handle the page changes on the front-end)
+    const indexPath = path.join(__dirname, '../public/jkp/src', 'index.html');
+    console.log(indexPath);
+    res.sendFile(indexPath); // load the single view file (angular will handle the page changes on the front-end)
 });
 
 module.exports = router;
