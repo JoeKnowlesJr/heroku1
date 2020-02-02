715886e9b1cda14a2a79ee84b743ae085adddfaa
Joe Knowles <josephbaderknowlesjr@gmail.com>
2020-02-01 22:33:18 -0600

    'testing'

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+mongoose.connect('mongodb+srv://jbk:jbkjbk@cluster0-anrfc.mongodb.net/test?retryWrites=true&w=majority');
+console.log(mongoose);
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
--- routes/register.js
+++
@@ -1,9 +0,0 @@
-// const express = require('express');
-// const router = express.Router();
-//
-// /* GET signup page. */
-// router.get('/register', function(req, res, next) {
-//     res.render('register', { title: 'Express' });
-// });
-//
-// module.exports = router;
