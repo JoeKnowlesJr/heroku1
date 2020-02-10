2f09e1f1bc20bb5a32818769497eb47ae47d5430
Joe Knowles <josephbaderknowlesjr@gmail.com>
2020-02-10 14:15:02 -0600

    'testing'

--- db.js
+++ db.js
@@ -1,15 +1,30 @@
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb+srv://jbk:jbkjbk@cluster0-anrfc.mongodb.net/test?retryWrites=true&w=majority';
+const DB_CONNECTION = 'mongodb://uflc9c8z9wm8n6cxsrrs:azWGaGTdnQmY4XtZn9II@bryanxlnnmcitzv-mongodb.services.clever-cloud.com:27017/bryanxlnnmcitzv';
 const options = {
     keepAlive: true,
-    keepAliveInitialDelay: 300000,
+    keepAliveInitialDelay: 30000,
     useNewUrlParser: true
 };
 
-mongoose.connect(mongoURI, options);
+mongoose.connect(
+    DB_CONNECTION,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+        // dbName: 'jkp',
+        // user: 'jbk',
+        // pass: 'jbkjbk'
+
+    })
+    .then(console.log('Connected to DB!'))
+    .catch(err =>  {
+        console.log(err);
+    });
 
 mongoose.connection.on('connected', ()=>{
-    console.log('Mongoose default connection open to ' + mongoURI);
+    console.log('Mongoose default connection open to ' + DB_CONNECTION);
 });
 
 // If the connection throws an error
--- routes/frontend.js
+++ routes/frontend.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const router = express.Router();
-
-// application -------------------------------------------------------------
-router.get('*', function(req, res) {
-    console.log('Getting frontend');
-    const indexPath = path.join(__dirname, '../public/jkp/src', 'index.html');
-    console.log(indexPath);
-    res.sendFile(indexPath); // load the single view file (angular will handle the page changes on the front-end)
-});
-
-module.exports = router;
+// const express = require('express');
+// const router = express.Router();
+//
+// // application -------------------------------------------------------------
+// router.get('*', function(req, res) {
+//     console.log('Getting frontend');
+//     const indexPath = path.join(__dirname, '../public/jkp/src', 'index.html');
+//     console.log(indexPath);
+//     res.sendFile(indexPath); // load the single view file (angular will handle the page changes on the front-end)
+// });
+//
+// module.exports = router;
