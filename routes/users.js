592dad1bc98c36a643214405b0ef06161d5b85fc
Joe Knowles <josephbaderknowlesjr@gmail.com>
2020-02-11 15:18:43 -0600

    'testing'

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const User = require('../models/user');
 
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 const passport = require('passport');
 
 
