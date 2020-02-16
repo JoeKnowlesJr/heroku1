ec7c0f698caeb42413c7f8dc9df2315d808be0ed
Joe Knowles <josephbaderknowlesjr@gmail.com>
2020-02-16 00:31:46 -0600

    'testing'

---
+++ public/javascripts/clock.js
@@ -0,0 +1,17 @@
+function updateClock ( ) {
+    const currentTime = new Date();
+
+    let currentHours = currentTime.getHours();
+    let currentMinutes = currentTime.getMinutes();
+    let currentSeconds = currentTime.getSeconds();
+    currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
+    currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
+
+    const timeOfDay = (currentHours < 12) ? "AM" : "PM";
+
+    currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
+
+    currentHours = ( currentHours === 0 ) ? 12 : currentHours;
+
+    document.getElementById("clock").firstChild.nodeValue = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
+}
